7b68fcf390e1215ccaf09bf2a0df949691ffda94
Harryc <harrycolyer@hotmail.co.uk>
2023-08-09 07:37:28 +0100

    initial commit

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import react, { StrictMode } from 'react'
+import ReactDOM from 'react-dom'
+import App from "./App"
+
+ReactDOM.render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+    document.getElementById("root")
+)
